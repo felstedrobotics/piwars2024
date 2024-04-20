779c2c5c8fafb0ab90358f79cab7931b851f83d5
arozx <jcooke@felsted.org>
2024-04-20 11:20:06 +0100

    simplify controller

--- controler/xbox_controler_control.py
+++ controler/xbox_controler_control.py
@@ -1,9 +1,11 @@
-from time import sleep
-from approxeng.input.controllers import find_matching_controllers, ControllerRequirement
-from approxeng.input.selectbinder import bind_controllers
-import redboard
 import curses
+from time import sleep
 
+import redboard
+from approxeng.input.controllers import ControllerRequirement, find_matching_controllers
+from approxeng.input.selectbinder import bind_controllers
+
+r = redboard.RedBoard()
 discovery = None
 
 # Connect to controller
@@ -30,7 +32,6 @@ def main(stdscr):
     motor2 = 0
 
     while True:
-
         curses.halfdelay(1)
 
         try:
@@ -38,29 +39,17 @@ def main(stdscr):
                 print("Left stick: x={}, y={}\n".format(lx, ly))
                 print("Right stick: x={}, y={}\n".format(rx, ry))
                 if lx > 0:
+                    motor1, motor2 = 0, 0
                     motor1 += 100 * lx
                     motor2 -= 100 * lx
                 if lx < 0:
+                    motor1, motor2 = 0, 0
                     motor1 -= 100 * lx
                     motor2 += 100 * lx
 
                 redboard.M1(motor1)
                 redboard.M2(motor2)
-
-                # Control motors based on right joystick
-                if ry > 0:
-                    motor1 += 100 * ry
-                    motor2 += 100 * ry
-                if ry < 0:
-                    motor1 -= 100 * ry
-                    motor2 -= 100 * ry
-
-                redboard.M1(motor1)
-                redboard.M2(motor2)
-
                 sleep(0.1)
-                motor1 = 0
-                motor2 = 0
         except StopIteration:
             # Raised when the stream ends
             print("Controller disconnected")
